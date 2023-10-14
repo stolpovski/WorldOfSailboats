15a7b206b23db88274e09a5753193db7fb294858
stolpovski <stolpovski@gmail.com>
2023-10-14 23:41:30 +0300

    boat

---
+++ Assets/Scripts/App.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class App : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/FPS.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPS : MonoBehaviour
+{
+    float elapsed = 0f;
+    Text txt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        txt = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsed += Time.deltaTime;
+        if (elapsed >= 1f)
+        {
+            elapsed %= 1f;
+            int avgFrameRate = (int)(1f / Time.unscaledDeltaTime);
+
+            txt.text = avgFrameRate.ToString();
+        }
+    }
+}
